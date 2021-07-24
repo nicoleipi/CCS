3fee8f7b79879d86d5690aa24560cc7013c5157c
nicoleipi <pinicolei@gmail.com>
2021-07-23 21:14:30 -0700

    update

--- info-extract.sh
+++ info-extract.sh
@@ -4,5 +4,17 @@ input_file=$1
 output_file=$2
 
 aline=`cat ${input_file} | grep "Sum of electronic and zero-point Energies"`
+var_ezpe=`echo ${aline} | cut -d " " -f 7`
+echo ${var_ezpe}
+
+aline=`cat ${input_file} | grep "Sum of electronic and thermal Energies"`
 var_u=`echo ${aline} | cut -d " " -f 7`
 echo ${var_u}
+
+aline=`cat ${input_file} | grep "Sum of electronic and thermal Enthalpies"`
+var_h=`echo ${aline} | cut -d " " -f 7`
+echo ${var_h}
+
+aline=`cat ${input_file} | grep "Sum of electronic and thermal Free Energies"`
+var_g=`echo ${aline} | cut -d " " -f 8`
+echo ${var_g}
