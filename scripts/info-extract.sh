4cbb35fc57456c834916333d734008ed0c4b6190
nicoleipi <pinicolei@gmail.com>
2021-07-30 17:37:19 -0700

    generate seperate csv files

--- scripts/info-extract.sh
+++ scripts/info-extract.sh
@@ -1,45 +1,61 @@
 #!/bin/bash
 
-input_file=$1
-output_file=$2
+in_dir=${1}
 
 basis_sets=("6-31G(d)" "6-31G(d,p)" "6-31+G(d,p)" "6-311G(d,p)" "6-311++G(d,p)" "6-311G(2df,2pd)" "6-311++G(2df,2pd)")
 
-for i in ${!basis_sets[@]};
-do 
-    num=0
-    basis=${basis_sets[$i]}
-    num=`grep ${basis} ${input_file} | wc -l`
-    if [ ${num} -gt 0 ]; then
-        basis_set=${basis}
-    fi
-done
-#echo ${basis_set}
+find ${in_dir} -name "*.log" > log_files.txt
+exec_path=/aerosol/users/nicole/CCS
 
-aline=`cat ${input_file} | grep "Sum of electronic and zero-point Energies"`
-var_ezpe=`echo ${aline} | cut -d " " -f 7`
-#echo ${var_ezpe}
+while read -r aline; do
+    file_path=`dirname ${aline}`
+    file_name=`basename ${aline}`
 
-aline=`cat ${input_file} | grep "Sum of electronic and thermal Energies"`
-var_u=`echo ${aline} | cut -d " " -f 7`
-#echo ${var_u}
+    cd ${exec_path}/${file_path}
 
-aline=`cat ${input_file} | grep "Sum of electronic and thermal Enthalpies"`
-var_h=`echo ${aline} | cut -d " " -f 7`
-#echo ${var_h}
+    for i in ${!basis_sets[@]}
+    do
+        num=0
+        basis=${basis_sets[$i]}
+        num=`grep ${basis} ${file_name} | wc -l`
+        if [ ${num} -gt 0 ]; then
+            basis_set=${basis}
+        fi
+    done
+    #echo ${basis_set}
 
-aline=`cat ${input_file} | grep "Sum of electronic and thermal Free Energies"`
-var_g=`echo ${aline} | cut -d " " -f 8`
-#echo ${var_g}
+    full_line=`cat ${file_name} | grep -i "Sum of electronic and zero-point Energies"`
+    var_ezpe=`echo ${full_line} | cut -d " " -f 7`
+    #echo ${var_ezpe}
 
-aline=`cat ${input_file} | grep 'HF='`
-search="HF="
-prefix=${aline%%${search}*}
-num=${#prefix}
-let "num=num+3"
-rest_of_line=${aline:${num}}
-search='\\'
-var_e=${rest_of_line%%${search}*}
-#echo ${var_e}
+    full_line=`cat ${file_name} | grep -i "Sum of electronic and thermal Energies"`
+    var_u=`echo ${full_line} | cut -d " " -f 7`
+    #echo ${var_u}
 
-echo "${basis_set}, ${var_ezpe}, ${var_u}, ${var_h}, ${var_g}, ${var_e}" >> ${output_file}
+    full_line=`cat ${file_name} | grep -i "Sum of electronic and thermal Enthalpies"`
+    var_h=`echo ${full_line} | cut -d " " -f 7`
+    #echo ${var_h}
+
+    full_line=`cat ${file_name} | grep -i "Sum of electronic and thermal Free Energies"`
+    var_g=`echo ${full_line} | cut -d " " -f 8`
+    #echo ${var_g}
+
+    full_line=`cat ${file_name} | grep 'HF='`
+    search="HF="
+    prefix=${full_line%%${search}*}
+    num=${#prefix}
+    let "num=num+3"
+    rest_of_line=${full_line:${num}}
+    search='\\'
+    unformat_e=`echo ${rest_of_line%%${search}*}`
+    var_e=`echo "${unformat_e}"`
+    #echo ${var_e}
+
+    name=`basename ${file_name} .log`
+
+    echo "${name}, ${basis_set}, ${var_ezpe}, ${var_u}, ${var_h}, ${var_g}, ${var_e}" >> ${exec_path}/BY_model4_data.csv
+
+done <${exec_path}/log_files.txt
+
+sort -k 2n ${exec_path}/BY_model4_data.csv > tmp.txt
+mv tmp.txt ${exec_path}/BY_model4_data.csv
