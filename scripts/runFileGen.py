ee443baf0ef8d0a665a03b6397be0201cccf2bfe
Nicole Pi <pinicolei@gmail.com>
2022-06-22 09:42:10 +0100

    resolve merge conflicts

--- scripts/runFileGen.py
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 fileGen.py -i /aerosol/users/nicole/CCS/structures/molecule1 -c 0 -s 1 -o /aerosol/users/nicole/CCS/output
-python3 fileGen.py -i /aerosol/users/nicole/CCS/structures/molecule4 -c 1 -s 1 -o /aerosol/users/nicole/CCS/output
-
