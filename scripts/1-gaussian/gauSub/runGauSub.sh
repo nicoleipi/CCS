f0309a9ffd96424637134b44f281a277c4085f34
Nicole Pi <pinicolei@gmail.com>
2022-06-24 14:15:32 -0700

    organize scripts folder

--- scripts/1-gaussian/gauSub/runGauSub.sh
+++ scripts/1-gaussian/gauSub/runGauSub.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
 
-#input directory variable
-in_dir=${1}
+#
+# directory structure:
+#  model/method/basis/molecule_foo.com
+#  model/method/basis/molecule_bar.com
+#
+full_path=/aerosol/users/nicole/CCS
+initial_basis="6-31Gd"
+basis_sets="6-31Gdp 6-31+Gdp 6-311Gdp 6-311++Gdp 6-311G2df2pd 6-311++G2df2pd"
 
-#sets the path the script is initiated
-exec_path=/aerosol/users/nicole/CCS
+function sub_model_method() {
+    model=$1
+    method=$2
 
-#makes a list of all the .com files in the directory
-find ${in_dir} -name "*.com" > list.txt
+    initial_path=${full_path}/${model}/${method}/${initial_basis}
+    cd ${initial_path}
+    # Loop over all molecules
+    for input in ./*.com
+    do
+        # Launch job for inital basis
+        cd ${initial_path}
+        job=$(basename $input)
+        chk_name=${job:9:-4}.chk
+        # Get job id - need to check what string is returned by launch script
+        job_id=$(${full_path}/gau-sub.sh ${job})
+        job_id=$(echo $job_id | awk '{print $4}')
 
-while read -r aline; do
-    #gets file path of each file in the list
-    file_path=`dirname ${aline}`
-    #gets the name of each file in the list
-    file_name=`basename ${aline}`
+        # Launch jobs for all other basis sets
+        # Submitted job now must
+        # i) know its dependency
+        # ii) be able to copy the required data from dependent job (i.e. checkpoint file)
+        for basis in ${basis_sets}
+        do
+            cd ${full_path}/${model}/${method}/${basis}
+            ${full_path}/gauSub.sh -c ${initial_path}/${chk_name} -d ${job_id} ${job}
+        done
+    done
+}
 
-    #enters the directory the file is in
-    cd ${exec_path}/${file_path}
-
-    #runs the job submission script
-    ${exec_path}/gau-sub.sh "${file_name}"
-done <list.txt
+sub_model_method model1 B3LYP
+sub_model_method model4 B3LYP
+sub_model_method model1 D3BJ
+sub_model_method model4 D3BJ
