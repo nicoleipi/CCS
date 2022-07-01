b27c719a229be4578dce47b2cd90ccf1b0b0b609
Nicole Pi <pinicolei@gmail.com>
2022-07-01 01:53:14 -0700

    gauSub v2

--- scripts/1-gaussian/gauSub/runGauSub.py
+++ scripts/1-gaussian/gauSub/runGauSub.py
@@ -17,7 +17,8 @@ parser.add_argument("-i", "--input", type=str, required=True, help="Directory co
 parser.add_argument("-o", "--output", type=str, required=True, help="Directory that will contain Gaussian .log files")
 args = parser.parse_args()
 
-# variables 
+# variables
+input = args.input
 script_path = os.getcwd()
 method = ["B3LYP", "D3BJ"]
 initial_basis = "6-31Gd"
@@ -25,12 +26,33 @@ basis_sets = ["6-31Gdp", "6-31+Gdp", "6-311Gdp", "6-311++Gdp", "6-311G2df2pd", "
 
 for i in range(len(method)):
     a_method = method[i]
-    initial_file_list = glob.glob('a_method+"_"+initial_basis+"*.com"')
-        
+    file_pattern = input+"/"+a_method+"_"+initial_basis+"_*.com"
+    initial_file_list = glob.glob(file_pattern)
+    
     for j in range(len(initial_file_list)):
-        a_file = initial_file_list[j]
+        a_file = os.path.basename(initial_file_list[j])
         a_filename = os.path.splitext(a_file)[0]
         var = a_filename.split("_")
         isomer = var [2]
         print(isomer)
+        os.chdir(input)
+        run_command = "bash "+script_path+"/gauSub.sh "+a_filename+".com > /tmp/job_id"
+        #os.system(run_command)
+        print(run_command)
+
+        with open("/tmp/job_id") as f:
+            lines = f.read()
+            first_line = lines.split('\n', 1)[0]
+            split = first_line.split(" ")
+            job_id = split [3]
+            job_id = str(job_id)
+        print(job_id)
+
+        for k in range(len(basis_sets)):
+            dep_command = "bash "+script_path+"/gauSub.sh -c "+a_filename+".chk -d "+job_id+" "+method[i]+"_"+basis_sets[k]+"_"+isomer+".com"
+            #os.system(dep_command)
+            print(dep_command)
+        
+        #os.system(run_command)
+        #job_id=$(${full_path}/gau-sub.sh ${job})
 
